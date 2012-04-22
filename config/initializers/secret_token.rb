ad1f8518961a0461e333b059a82ab01fa3443927
TyT <tyster100@gmail.com>
2012-04-21 22:15:10 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TyDemoApp::Application.config.secret_token = '55f5bc79950374494f0d204726dc0299720700e7fb97a8e5cbabfeb605e374b24048f1b098facd815fc0b221fa153edb1e5784295ec78c0ba6a0f36e8337c2f9'
